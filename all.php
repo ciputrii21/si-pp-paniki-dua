cb29d285663cb19760c7c8e360e6f58e570c641a
ciputrii21 <shalomputri@elektro.polimdo.ac.id>
2023-12-11 17:45:12 +0800

    first commit

---
+++ all.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Personal Website</title>
+   <link rel="stylesheet" href="fontawesome/css/all.css">
+    <link rel="stylesheet" type="text/css" href="css/all.css"/>
+</head>
+ 
+<body>
+        <!--- navigation --->
+        <nav>
+            <!--- logo --->
+            <div class="logo"><img src="img/logo.png"> 
+            <p>Kelurahan Paniki Dua</p>
+            </div>
+            <!--- menu --->
+            <style>
+                /* Ganti warna teks untuk tautan yang aktif */
+                ul li a.active {
+                    color: #000000; /* Ganti dengan kode warna yang diinginkan */
+                }
+
+            </style>
+            <ul>
+                <li><a href="#beranda" class="active">Beranda</a></li>
+                <li><a href="#pelayanan" class="active">Pelayanan</a></li>
+                <li><a href="#tentang" class="active">Tentang</a></li>
+                <li><a href="#kontak" class="active">Kontak</a></li>
+                <li><a href="index.php" class="active">Surat</a></li>
+            </ul>
+        </nav>
+
+        <section id = "beranda">
+        <div class="background">
+                <img src="img/kelurahan.png" width="100%" height="100%">
+            </div>
+    </section>
+
+    <section id = pelayanan>
+        <div class="services">
+        <!--text-->
+        <div class="services-text ">
+            <p>Pelayanan</p>
+            <p>Kelurahan adalah unit pemerintahan tingkat kelima di Indonesia, setingkat di bawah kecamatan. Kelurahan memiliki tugas dalam penyelenggaraan pemerintahan, pemberdayaan masyarakat, dan pelayanan umum di tingkat kelurahan. Untuk pelayanan publik di Kantor Kelurahan Pedalangan buka pada hari:
+                <br> <br> Hari Senin s.d Kamis Jam 07.45 WIB – 17.00 WITA
+                <br> Hari Jumat Jam 07.00 WIB – 11.15 WIB
+                <br> Hari Sabtu dan Minggu Libur.</p>
+        </div>
+         <h2>Data Pembuatan Surat</h2>
+         <div class="table-container">
+    <table>
+        <tr>
+            <th>No.</th>
+            <th>Nama Surat</th>
+            <th>Syarat</th>
+        </tr>
+ 
+        <tr>
+            <td align="center">01</td>
+            <td align="center">Surat Keterangan Penduduk</td>
+            <td>Rekomendasi Kepala Lingkungan, Fotocopy KTP, Fotocopy KK, Fotocopy Lunas Pajak Tahun Berjalan, Lunas Retribusi Sampah Tahun Berjalan</td>
+        </tr>
+ 
+        <tr>
+            <td align="center">02</td>
+            <td align="center">Surat Keterangan Domisili Penduduk</td>
+            <td>Rekomendasi Kepala Lingkungan, Fotocopy KTP, Fotocopy KK, Fotocopy KTP (Lampirkan Asli) Bagi Penduduk Pendatang / Status Kost, Pelajar, Sekolat, Atau Kerja, Fotocopy Lunas Pajak Tahun Berjalan, Lunas Retribusi Sampah Tahun Berjalan</td>
+        </tr>
+
+        <tr>
+            <td align="center">03</td>
+            <td align="center">Surat Keterangan Domisili Usaha</td>
+            <td>Rekomendasi Kepala Lingkungan, Fotocopy KTP / KK Pelaku Usaha, Fotocopy Akte Pendirian Usaha, Fotocopy SITU / SIUP, Fotocopy Lunas Pajak Tahun Berjalan, Lunas Retribusi Sampah Tahun Berjalan</td>
+        </tr>
+    </table>
+</div>
+</section>
+
+<section id = "tentang">
+    <div class="visimisi">
+        <!--text-->
+            <h1>VISI</h1>
+            <p>Manado Maju Dan Sejahtera Sebagai Beranda Sulawesi Utara Dan Indonesia Ke Asia Pasifik</p>
+            <h1>MISI</h1>
+            <p>1. Peningkatan Kualitas Manusia Kota Manado;</p>
+            <p>2. Penguatan Ekonomi Kota Yang Bertumpu Kepada Industri Jasa, Perdagangan, Dan Pariwisata;</p>
+            <p>3. Pembangunan Infrastruktur, Penataan Kota, Dan Perluasan Konektivitas;</p>
+            <p>4. Pembangunan Daerah Yang Berkelanjutan;</p>
+            <p>5. Pemerintah Yang Baik Dan Bersih Di DUkung Oleh Sinergitas Dan Daerah;</p>
+        </div>
+    <div class="struktur">
+        <h1>STRUKTUR PEMERINTAH KELURAHAN PANIKI DUA</h1>
+        <h1>KECAMATAN MAPANGET</h1>
+        <img src="img/struktur.png">
+    </div>
+</section>
+
+        <section id="kontak">
+     <footer class="footer">
+        <div class="footer-left">
+            <h3>Kelurahan Paniki Dua</h3>
+            <div class="credit-cards">
+                <img src="img/visa.png" alt="">
+                <img src="img/mastercard.png" alt="">
+                <img src="img/paypal.png" alt="">
+            </div>
+            <p class="footer-copyright">2023 Kelurahan Paniki Dua</p>
+        </div>
+    
+        <!-- Arsip -->
+        <!-- <section id="arsip">
+     <footer class="footer">
+        <div class="footer-left">
+            <h3>Kelurahan Paniki Dua</h3>
+            <div class="credit-cards">
+                <img src="img/visa.png" alt="">
+                <img src="img/mastercard.png" alt="">
+                <img src="img/paypal.png" alt="">
+            </div>
+            <p class="footer-copyright">2023 Kelurahan Paniki Dua</p>
+        </div> -->
+
+        <div class="footer-center">
+            <div>
+                <i class="fa fa-map-marker"></i>
+                <p><span>Indonesia</span> Manado, Sulawesi Utara</p>
+            </div>
+            <div>
+                <i class="fa fa-phone"></i>
+                <p>+6285298584172</p>
+            </div>
+            <div>
+                <i class="fa fa-envelope"></i>
+                <p><a href="https://mail.google.com/mail/Kelurahanpaniki2@gmail.com">Kelurahanpaniki2@gmail.com</a></p>
+            </div>
+        </div>
+
+        <div class="footer-right">
+            <p class="footer-about">
+                <span>Pesan</span>
+                Diharapkan kedepannya website ini bisa membantu para masyarakat dalam pelayanan pembuatan surat-surat secara digital.
+            </p>
+
+            <div class="footer-media">
+                <a href="whatsapp://send?text=&phone=+6285298584172"><i class="fa-brands fa-square-whatsapp"></i>
+                <a href="https://www.facebook.com/kelurahanpanikiduamapanget"><i class="fa-brands fa-facebook-f"></i>
+                <a href="https://youtube.com/@KelurahanPanikiDua?si=4MI4GC6--On285Xg"><i class="fab fa-youtube"></i></a>
+            </div>
+
+        </div>
+
+        <div class="footer-bottom">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15953.71114093026!2d124.9200297!3d1.5127413!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3287a091af3b0b91%3A0x7f9213387a83dbf6!2sKantor%20Kelurahan%20Paniki%20Dua!5e0!3m2!1sid!2sid!4v1702116776732!5m2!1sid!2sid" width="100%" height="300px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </footer>
+
+</section>
+</div>
+
+<div class="social">
+        <a href="whatsapp://send?text=&phone=+6285298584172"><i class="fa-brands fa-square-whatsapp"></i>
+        <a href="https://www.facebook.com/kelurahanpanikiduamapanget"><i class="fa-brands fa-facebook-f"></i>
+        <a href="https://youtube.com/@KelurahanPanikiDua?si=4MI4GC6--On285Xg"><i class="fab fa-youtube"></i></a>
+    </div>
+
+</body>
+ 
+</html>
